66401eb09ea0e1a878228ece8ad5eadd8fbf316e
Leidy Ward <leidyward@Leidys-MacBook-Air.local>
2018-09-30 12:57:19 -0500

    woo

--- src/Project_Gutenberg.java
+++ src/Project_Gutenberg.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Project_Gutenberg {
 
@@ -35,13 +36,77 @@ public class Project_Gutenberg {
 		}
 	}
 	
-	int getTotalUniqueWords() {
-		return 0;
+	static void getTotalUniqueWords() throws FileNotFoundException {
+		File file = new File("src/Book.txt");
+		ArrayList list = new ArrayList();
+		HashMap<String, Integer> words = new HashMap<String, Integer>();
+		Scanner scan = new Scanner(file);
+		int i = 0;
+		
+		while(scan.hasNext()) {
+			String str = scan.next();
+			//System.out.println(str);
+			list.add(str);
+		}
+		Iterator j = list.iterator();
+		while(j.hasNext()) {
+			i++;
+			words.put((String)j.next(), i);
+			//System.out.println(words);
+		}
+		Set<Object> uniqueWords = new HashSet<Object>(words.values());
+		
+		System.out.println("Number of unique words: " + uniqueWords.size());
 	}
-	String get20MostFrequentWords(){
-		return "";
+	
+	static String get20MostFrequentWords(){
+		
+		System.out.println("\nThe 20 most frequent words:\n");
+		
+		LinkedHashMap<String, Integer> countWord = new LinkedHashMap<String, Integer>();
+		try {
+			BufferedReader read = new BufferedReader(new FileReader("src/Book.txt"));
+			String s;
+			
+			while((s = read.readLine()) != null) {
+				s = s.toLowerCase();
+				String[] words = s.split("\\s+");	//split line at whitespace
+				
+				for(String w : words) {
+					if(w.length() == 0)
+						continue;
+					Integer occur = countWord.get(w);
+					if(occur == null)
+						occur = 1;
+					else
+						occur++;
+					countWord.put(w, occur);
+				}
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		ArrayList<Integer> val = new ArrayList<Integer>();
+		val.addAll(countWord.values());
+		
+		Collections.sort(val, Collections.reverseOrder());
+		
+		int last = -1;
+		for(Integer i : val.subList(0, 19)) {
+			if(last == i)	//avoids duplicates
+				continue;
+			last = i;
+	
+			for(String str : countWord.keySet()) {
+				if(countWord.get(str) == i)		//i = number of occurrences
+					System.out.println(str + " " + i); 
+			}
+		}
+		return null;
 	}
-	String get20MostInterestingFrequentWords(){
+	/*String get20MostInterestingFrequentWords(){
 		return "";
 	}
 	String get20LeastFrequentWords(){
@@ -56,15 +121,17 @@ public class Project_Gutenberg {
 	String generateSentence(){
 		return "";
 	}
-	//List<String> getAutocompleteSentence(String startOfSentence){
-	//	return "";
-	//}
-	//String findClosestMatchingQuote(){
-	//	return "";
-	//}
-	
-	public static void main(String[] args) {
+	List<String> getAutocompleteSentence(String startOfSentence){
+		return "";
+	}
+	String findClosestMatchingQuote(){
+		return "";
+	}
+	*/
+	public static void main(String[] args) throws FileNotFoundException {
 		Project_Gutenberg.getTotalNumberOfWords();
+		Project_Gutenberg.getTotalUniqueWords();
+		Project_Gutenberg.get20MostFrequentWords();
 
 	}
 
